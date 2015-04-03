a0da72e3855d4bfef44894909ca75bf510b91d53
Nathan Appere <nathan.appere@gmail.com>
2015-04-02 18:45:27 -0700

    Update .env.mine loading order

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -3,9 +3,9 @@ require "dotenv/deployment/version"
 
 # Load defaults from .env or *.env in config
 Dotenv.load '.env'
-Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env") if defined?(Rails)
-
 Dotenv.overload ".env.mine"
+
+Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env") if defined?(Rails)
 Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env.mine") if defined?(Rails)
 
 # Override any existing variables if an environment-specific file exists
