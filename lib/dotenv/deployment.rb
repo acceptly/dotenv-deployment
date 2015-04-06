edb7a8157c836e97dfeb05954c52254c86c5bbff
Nathan Appere <nathan.appere@gmail.com>
2015-04-06 16:09:13 -0700

    Adds basic logging

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -1,26 +1,54 @@
 require "dotenv"
 require "dotenv/deployment/version"
 
-rails_root = Rails.root || Dir.pwd if defined?(Rails)
+module Dotenv
+  module Deployment
+    class << self
 
-# Load defaults from .env or *.env in config
-Dotenv.load('.env')
-Dotenv.load(*Dir.glob("#{rails_root}/config/**/*.env")) if defined?(Rails)
+      def log(*files)
+        files.each do |file|
+          puts "[DOTENV] Trying to load #{file}"
+        end
+      end
 
-# Override any existing variables if an environment-specific file exists
-if environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
-  environment = 'circle' if ENV['CIRCLECI']
+      def load(*files)
+        log         *files
+        Dotenv.load *files
+      end
 
-  Dotenv.overload(".env.#{environment}")
-  if defined?(Rails)
-    config_env = Dir.glob("#{rails_root}/config/**/*.env.#{environment}", File::FNM_DOTMATCH)
-    Dotenv.overload(*config_env) if config_env.any?
+      def overload(*files)
+        log             *files
+        Dotenv.overload *files
+      end
+
+      def run!
+        rails_root = Rails.root || Dir.pwd if defined?(Rails)
+
+        # Load defaults from .env or *.env in config
+        load('.env')
+        load(*Dir.glob("#{rails_root}/config/**/*.env")) if defined?(Rails)
+
+        # Override any existing variables if an environment-specific file exists
+        if environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
+          environment = 'circle' if ENV['CIRCLECI']
+
+          overload(".env.#{environment}")
+          if defined?(Rails)
+            config_env = Dir.glob("#{rails_root}/config/**/*.env.#{environment}", File::FNM_DOTMATCH)
+            overload(*config_env) if config_env.any?
+          end
+
+          overload(".env.#{environment}.mine")
+          if defined?(Rails)
+            config_env = Dir.glob("#{rails_root}/config/**/*.env.#{environment}.mine", File::FNM_DOTMATCH)
+            overload(*config_env) if config_env.any?
+          end
+        end
+      end
+
+    end
+
+    run!
+
   end
-
-  Dotenv.overload(".env.#{environment}.mine")
-  if defined?(Rails)
-    config_env = Dir.glob("#{rails_root}/config/**/*.env.#{environment}.mine", File::FNM_DOTMATCH)
-    Dotenv.overload(*config_env) if config_env.any?
-  end
-
 end
--- lib/dotenv/deployment/version.rb
+++ lib/dotenv/deployment/version.rb
@@ -1,5 +1,5 @@
 module Dotenv
   module Deployment
-    VERSION = "0.4.0"
+    VERSION = "0.4.1"
   end
 end
